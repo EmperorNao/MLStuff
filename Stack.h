52f71d615dd286a4d5017a814103ad949f0254f2
EmperorNao <asgasgagagwet@gmail.com>
2020-12-26 11:15:03 +0300

    Stack
    Interface for stack, based on vector

---
+++ Stack.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#pragma once
+#include <iostream>
+#include <vector>
+
+template <class T>
+class Stack
+{
+
+private:
+
+	std::vector<T> stack;
+
+public:
+
+	Stack() {  };
+	bool is_empty();
+	void push(T x);
+	T pop();
+	T top();
+
+};
+
+template <typename T>
+bool Stack<T>::is_empty() {
+
+	return stack.size() == 0;
+
+}
+
+template <typename T>
+void Stack<T>::push(T x) {
+
+	stack.push_back(x);
+
+}
+
+template <typename T>
+T Stack<T>::pop() {
+
+	if (!this->is_empty()) {
+
+		auto elem = stack[stack.size() - 1];
+		stack.pop_back();
+		return elem;
+
+	}
+
+}
+
+template <typename T>
+T Stack<T>::top() {
+
+	if (!this->is_empty()) {
+
+		return stack[stack.size() - 1];
+
+	}
+
+}
