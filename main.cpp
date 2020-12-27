73f424b489de7b62a20deb35a0755916da9ed878
EmperorNao <asgasgagagwet@gmail.com>
2020-12-27 16:18:20 +0300

    Deleted testing, only methods using

--- main.cpp
+++ main.cpp
@@ -1,99 +1,11 @@
-#include <iostream>
-#include "grad.h"
-#include <cmath>
-
-
-double test_func(double x) {
-
-	return (pow(3, -x) + pow(3, x - 2) + 2);
-	//return x * x + 3 * x - 4;
-
-}
-
-double test_deriv(double x) {
-
-	return (pow(3, -x - 2) * (pow(9, x) - 9) * log(3));
-
-}
-
-double autog(double x) {
-
-	double eps = 0.00001;
-	return (test_func(x + eps) - test_func(x)) / eps;
-
-}
-
-double test_func_n_dim(std::vector<double> x) {
-
-	return (x[0] - 2) * (x[0] - 2) / 9 + (x[1] + 3) * (x[1] + 3)/ 4 + 5;
-
-}
-
-std::vector<double> test_deriv_n_dim(std::vector<double> x) {
-
-	std::vector<double> t(2);
-	t[0] = 2 * (x[0] - 2) / 9; 
-	t[1] = (x[1] + 3) / 2;
-	return t;
-
-}
+#include "GradientsTesting.h"
 
 int main() {
 
-	// old test for one-dimensional descent testing
-	/*
+	TestsGradientDescents test;
+	test.test_simple_gradient_descent_1d();
+	test.test_simple_gradient_descent_2d();
 
-	std::cout << "F(x)=" << " 3^(-x) + 3^(x-2) + 2"<< std::endl;
-	double x_min = grad_descent_1d(test_func, autog, 0, 0.01, 0.00001, true);
-	std::cout << "x_min = " << x_min << std::endl;
-	std::cout << "F(x_min) = " << test_func(x_min) << std::endl << std::endl;
-	auto min = grad_descent(test_func_n_dim, test_deriv_n_dim, std::vector<double>(2, 0) , true);
-	for (int i = 0; i < min.size(); i++) {
-
-		std::cout << "x_" << i << " = " << min[i] << std::endl;
-
-	}
-	std::cout << "F(x_min) = " << test_func_n_dim(min) << std::endl;
-
-	*/
-
-	std::string s = "3^(~x) + 3^(x-2) + 2";
-	std::cout << "F(x) = " << s << std::endl;
-	ArithmeticalExpression expression;
-	expression.set_formula(s); 
-
-	config config;
-
-	std::vector<double> min = grad_descent(expression, config);
-
-	for (int i = 0; i < min.size(); i++) {
-
-		std::cout << "x_"<< i <<" = " << min[i] << std::endl;
-
-	}
-
-	std::cout << std::endl;
-	std::cout << "F(x) = " << "(x-2)^2/9 + (y+3)^2/4" << std::endl;
-	config.number_of_variables = 2;
-
-	min = grad_descent(test_func_n_dim, test_deriv_n_dim, config);
-	for (int i = 0; i < min.size(); i++) {
-
-		std::cout << "x_" << i << " = " << min[i] << std::endl;
-
-	}
-	std::cout << "F(x_min) = " << test_func_n_dim(min) << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "F(x) = " << "(x-2)^2/9 + (y+3)^2/4" << std::endl;
-
-	min = grad_descent(test_func_n_dim, config);
-	for (int i = 0; i < min.size(); i++) {
-
-		std::cout << "x_" << i << " = " << min[i] << std::endl;
-
-	}
-	std::cout << "F(x_min) = " << test_func_n_dim(min) << std::endl;
-
+	return 0;
 
 }
